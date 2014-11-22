caef05dec152ded9088484108ec6764dd2faf66d
ZNANAA <10113009@users.noreply.github.com>
2014-11-23 00:45:06 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,74 @@
-## mycomments
-## functions specificatopndo
-
-mymakeCacheMatrix <- function(x = matrix()) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get, setmean = setmean, getmean = getmean)
 }
 
 
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+mymakeCacheMatrix <- function(m = matrix()) 
+{
+  i <- NULL
+  
+  set <- function( matrix ) 
+  {
+    ##  <<- can be used to assign a value to an object in an environment that is different from the current environment
+    m <<- matrix
+    i <<- NULL
+  }
+  
+  get <- function() 
+  {
+    m
+  }
+  
+  setInverse <- function(inverse) 
+  {
+    i <<- inverse
+  }
+  
+  getInverse <- function()
+  {
+    i
+  }
+
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+
+cacheSolve <- function(x, ...)
+{
+  m <- x$getInverse()
+  
+  if( !is.null(m) ) 
+  {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+
+  m <- solve(data) %*% data
+  
+  x$setInverse(m)
+
+  m
 }
